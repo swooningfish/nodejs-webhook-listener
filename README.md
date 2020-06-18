6fe2712eaf8f7dfcb5210c7f4e3bc9a362dad5a6
Greg Colley <greg.colley@gmail.com>
2020-06-18 22:02:16 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# .env
+
+Edit the .env file for setting port or token. 
+
+
+# Install Node modules
+npm install
+
+# Run the webhook listener
+node index.js
---
+++ index.js
@@ -0,0 +1,79 @@
+// Require express and body-parser
+const express = require("express")
+const bodyParser = require("body-parser")
+require('dotenv').config();
+
+// Initialize express and define a port
+const app = express()
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
+const token = process.env.TOKEN;
+
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json())
+
+app.post("/hook", (req, res) => {
+  console.log(req.body) // Call your action on the request here
+  res.status(200).end() // Responding is important
+})
+
+app.get('/', (req, res) => {
+    // check if verification token is correct
+    if (req.query.token !== token) {
+        return res.sendStatus(401);
+    }
+
+    // return challenge
+    return res.end('Webhook Example');
+    //return res.end(req.query.challenge);
+});
+
+app.get('/test', (req, res) => {
+    // check if verification token is correct
+    
+	const data = {
+        responses: [
+            {
+                query: req.query,
+				body: req.body
+            }
+        ]
+    };
+	
+	//console.log(req.body);
+	//console.log(req.query);
+    // return challenge
+    res.json(data);
+	
+    //return res.end(req.query.challenge);
+});
+
+//app.get('/', (req, res) => res.send('Webhook Example'))
+
+app.post('/bot', (req, res) => {
+    // check if verification token is correct
+    if (req.query.token !== token) {
+        return res.sendStatus(401);
+    }
+
+    // print request body
+    console.log(req.body);
+
+    // return a text response
+    const data = {
+        responses: [
+            {
+                type: 'text',
+                elements: ['Hi', 'Hello']
+            }
+        ]
+    };
+
+    res.json(data);
+});
+
+// Start express on the defined port
+app.listen(PORT, HOST);
+console.log(`🚀 Server running on port http://${HOST}:${PORT}`);
+
+
